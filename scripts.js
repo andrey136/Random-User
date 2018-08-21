6000488e503517845de852bcdd82742150b870ff
Andrey Mardash <andrey.mardash@gmail.com>
2018-08-21 22:05:02 +0300

    email

--- scripts.js
+++ scripts.js
@@ -1,10 +1,190 @@
-let div;
-function randomUser(){
+String.prototype.makeName = function(){
+    return ( this[0].toUpperCase() + this.slice(1) );
+};
+
+function work(){
+    let viewer = document.getElementById('main');
+    viewer.innerHTML = '';
+    viewer.setAttribute('class', '');
+    let spinner = document.createElement('div');
+    spinner.setAttribute('class','loader');
+    viewer.appendChild(spinner);
+
+}
+
+function call(){
+    getHttpList()
+        .then((resolve) => render(resolve))
+        .catch( (reject) => renderError(reject));
+}
+
+function renderError(message) {
+    let viewer = document.getElementById('main');
+    viewer.innerHTML = '';
+    viewer.setAttribute('class', 'viewer small');
+
+    let infoWatch = document.createElement('div');
+    infoWatch.setAttribute('class','plain');
+    infoWatch.appendChild(createUserInfo('Sorry,',message));
+    viewer.appendChild(infoWatch);
+
+    document.body.appendChild(viewer);
+}
+
+function render(lst){
+    let viewer = document.getElementById('main');
+    viewer.innerHTML = '';
+    viewer.setAttribute('class','viewer big');
+
+    viewer.appendChild(createUserPic(lst.picture.large));
+
+    let infoWatch = document.createElement('div');
+    infoWatch.setAttribute('class','plain');
+    infoWatch.appendChild(createUserInfo('Name:',  lst.name.title.makeName() + ' '
+        + lst.name.first.makeName() + ' '
+        + lst.name.last.makeName()));
+    infoWatch.appendChild(createUserInfo('Phone Number:', lst.phone.toString()));
+    infoWatch.appendChild(createUserInfo('E-mail:', lst.email));
+    infoWatch.appendChild(createUserInfo('Gender:', lst.gender.toString().makeName()));
+    infoWatch.appendChild(createUserInfo('City:',lst.location.city.makeName()));
+    infoWatch.appendChild(createUserInfo('State:',lst.location.state.makeName()));
+    viewer.appendChild(infoWatch);
+
+    document.body.appendChild(viewer);
+}
+
+function createUserInfo(field, text){
+    let webInfo = document.createElement('p');
+    webInfo.setAttribute('class','info');
+    webInfo.innerHTML = field + ' ' + text;
+    return webInfo;
+}
+
+function createUserPic(pic){
+    let innerDiv = document.createElement('div');
+    innerDiv .setAttribute('class','img');
+
+    let image = document.createElement('img');
+    image.setAttribute('src', pic);
+    innerDiv.appendChild(image);
+    return innerDiv;
+}
+
+function getHttpList(){
+    let newQuery = new XMLHttpRequest();
+    newQuery.open("GET", 'https://randomuser.me/api/', false);
+    newQuery.send( null );
+
+    let response = JSON.parse(newQuery.response);
+
+    return new Promise((resolve,reject)=>{
+
+        if ( (Math.random() < 0.5) && ( response.results[0].gender !== 'female')) {
+            console.log('bad luck');
+            return reject('something went wrong, try again later');
+        }
+        return resolve(response.results[0]);
+    })
+}String.prototype.makeName = function(){
+    return ( this[0].toUpperCase() + this.slice(1) );
+};
+
+function work(){
+    let viewer = document.getElementById('main');
+    viewer.innerHTML = '';
+    viewer.setAttribute('class', '');
+    let spinner = document.createElement('div');
+    spinner.setAttribute('class','loader');
+    viewer.appendChild(spinner);
+
+    setTimeout(call, 1000 + Math.ceil(Math.random()*1000) );
+}
+
+function call(){
+    getHttpList()
+        .then((resolve) => render(resolve))
+        .catch( (reject) => renderError(reject));
+}
+
+function renderError(message) {
+    let viewer = document.getElementById('main');
+    viewer.innerHTML = '';
+    viewer.setAttribute('class', 'viewer small');
+
+    let infoWatch = document.createElement('div');
+    infoWatch.setAttribute('class','plain');
+    infoWatch.appendChild(createUserInfo('Sorry,',message));
+    viewer.appendChild(infoWatch);
+
+    document.body.appendChild(viewer);
+}
+
+function render(lst){
+    let viewer = document.getElementById('main');
+    viewer.innerHTML = '';
+    viewer.setAttribute('class','viewer big');
+
+    viewer.appendChild(createUserPic(lst.picture.large));
+
+    let infoWatch = document.createElement('div');
+    infoWatch.setAttribute('class','plain');
+    infoWatch.appendChild(createUserInfo('Name:',  lst.name.title.makeName() + ' '
+        + lst.name.first.makeName() + ' '
+        + lst.name.last.makeName()));
+    infoWatch.appendChild(createUserInfo('Phone Number:', lst.phone.toString()));
+    infoWatch.appendChild(createUserInfo('E-mail:', lst.email));
+    infoWatch.appendChild(createUserInfo('Gender:', lst.gender.toString().makeName()));
+    infoWatch.appendChild(createUserInfo('City:',lst.location.city.makeName()));
+    infoWatch.appendChild(createUserInfo('State:',lst.location.state.makeName()));
+    viewer.appendChild(infoWatch);
+
+    document.body.appendChild(viewer);
+}
+
+function createUserInfo(field, text){
+    let webInfo = document.createElement('p');
+    webInfo.setAttribute('class','info');
+    webInfo.innerHTML = field + ' ' + text;
+    return webInfo;
+}
+
+function createUserPic(pic){
+    let innerDiv = document.createElement('div');
+    innerDiv .setAttribute('class','img');
+
+    let image = document.createElement('img');
+    image.setAttribute('src', pic);
+    innerDiv.appendChild(image);
+    return innerDiv;
+}
+
+function getHttpList(){
+    let newQuery = new XMLHttpRequest();
+    newQuery.open("GET", 'https://randomuser.me/api/', false);
+    newQuery.send( null );
+
+    let response = JSON.parse(newQuery.response);
+
+    return new Promise((resolve,reject)=>{
+
+        if ( (Math.random() < 0.5) && ( response.results[0].gender !== 'female')) {
+            console.log('bad luck');
+            return reject('something went wrong, try again later');
+        }
+        return resolve(response.results[0]);
+    })
+}
+
+/*function randomUser(){
     axios.get('https://randomuser.me/api')
         .then(function (response) {
-            // handle success
-            console.log(response.data);
-            render(response);//.data.results[0]
+
+            let i = 0;
+            render(response.data.results[0].email, i++);//.data.results[0]
+            render(response.data.results[0].name.first, i++);//.data.results[0]
+            render(response.data.results[0].name.last, i++);//.data.results[0]
+            render(response.data.results[0].password, i++);//.data.results[0]
+
         })
         .catch(function (error) {
             // handle error
@@ -13,14 +193,4 @@ function randomUser(){
         .then(function () {
             // always executed
         });
-}
-function render (obj){
-    for (let i = 0; i < 3; i++){
-        div = document.createElement('div');
-
-        div.innerHTML = obj.data.results[0].email;
-        console.log(obj);
-        document.getElementById('email').appendChild(div);
-    }
-
-}
+}*/
